2604e76ff74ae6464b6b81cb7e8c2dcbb7665dd4
SathrubaP <sathrubap.22ece@kongu.edu>
2024-12-28 09:06:28 +0530

    txt

---
+++ Variables.js
@@ -0,0 +1,12 @@
+// let student1 =new Object();
+// student1.name ="abi";
+// student1.age=20;
+// console.log(student1);
+
+let a=5;
+let b=a++ + --a;
+ let c=++b + b++;
+ a++;
+console.log(a);
+console.log(b);
+console.log(c);
